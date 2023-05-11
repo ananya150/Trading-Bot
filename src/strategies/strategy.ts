f9f36012a4742614c1a788b7b12bdde3bb3625a9
ananya150 <akblockchain15@gmail.com>
2023-05-12 00:50:48 +0530

    feat: added basic strategy

---
+++ src/strategies/strategy.ts
@@ -0,0 +1,41 @@
+import { IExchangeInterface } from '../interfaces/exchangeInterface';
+
+export class SImpleStategy {
+    private exchange: IExchangeInterface;
+    private baseCurrency: string;
+    private quoteCurrency: string;
+    private lowThreshold: number;
+    private highThreshold: number;
+
+    constructor(exchange: IExchangeInterface, baseCurrency: string, quoteCurrency: string, lowThreshold: number, highThreshold: number) {
+        this.exchange = exchange;
+        this.baseCurrency = baseCurrency;
+        this.quoteCurrency = quoteCurrency;
+        this.lowThreshold = lowThreshold;
+        this.highThreshold = highThreshold;
+    }
+
+    async execute() {
+
+        const symbol = `${this.baseCurrency}/${this.quoteCurrency}`;
+        const ticker = await this.exchange.fetchTicker(symbol);
+
+        if (ticker.last < this.lowThreshold) {
+
+            console.log(`Buying ${symbol} at ${ticker.last}`);
+
+            // TODO: Determine the amount to buy
+
+            await this.exchange.createOrder(symbol, 'buy', 1, ticker.last);
+
+          } else if (ticker.last > this.highThreshold) {
+
+            console.log(`Selling ${symbol} at ${ticker.last}`);
+
+            // TODO: Determine the amount to sell
+            
+            await this.exchange.createOrder(symbol, 'sell', 1, ticker.last);
+          }
+        }
+      
+    }
